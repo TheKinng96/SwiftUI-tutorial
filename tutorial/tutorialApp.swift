182fa0dc3483629bd61d64f626cbed79272d50ef
Your name <Your email>
2022-04-14 21:20:00 +0900

    Initial Commit

---
+++ tutorial/tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorialApp.swift
+//  tutorial
+//
+//  Created by Feng Yuan Yap on 2022/04/14.
+//
+
+import SwiftUI
+
+@main
+struct tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
