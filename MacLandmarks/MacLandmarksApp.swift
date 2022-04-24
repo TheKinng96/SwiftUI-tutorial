9f35b82961e6f412072d413c5ba99409eccfe31e
Yap Feng Yuan <fengyuan.yap@gmail.com>
2022-04-24 13:55:42 +0900

    removed test and added mac os app

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Feng Yuan Yap on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
